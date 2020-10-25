441fd7a2faf8ad5cf5e5d2c1a1e58289f732ab81
uday1331 <u3555276@connect.hku.hk>
2020-10-25 12:03:01 +0800

    server: add serializer for model Location

--- hotzone/models.py
+++ hotzone/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class Location(models.Model):
     def __str__(self):
-      return '{self.__class__.__name__}: ({self.x_coord}, {self.y_coord})'.format(self = self)
+        return '{self.__class__.__name__}: ({self.x_coord}, {self.y_coord})'.format(self = self)
 
     x_coord = models.IntegerField('X Co-ordinate')
     y_coord = models.IntegerField('Y Co-ordinate')
