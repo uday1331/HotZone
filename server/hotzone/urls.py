dacb53869195d60b497faa780665ba34bb971bc1
uday1331 <u3555276@connect.hku.hk>
2020-10-25 14:37:51 +0800

    server: basic rest setup

--- server/hotzone/urls.py
+++ server/hotzone/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('locations/', views.location_list),
+    path('locations/<int:pk>/', views.location_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/hotzone/views.py
+++ server/hotzone/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from hotzone.models import Location
+from hotzone.serializers import LocationSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the hotZone index.")
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def location_list(request, format = None):
+    if request.method == 'GET':
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes((permissions.AllowAny,))
+def location_detail(request, pk, format = None):
+    try:
+        location = Location.objects.get(pk=pk)
+    except Location.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = LocationSerializer(location)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = LocationSerializer(location, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        location.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
