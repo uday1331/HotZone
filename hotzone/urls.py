403d3395e08fd79c7b084fa321cac1ec233e9686
Uday Jain <47697048+uday1331@users.noreply.github.com>
2020-10-25 15:26:12 +0800

    server: setup rest endpoints for CRUD on Locations model  (#2)
    * server: basic rest setup
    
    * server: using classes for endpoints
    
    * server: using generics for location api requests

--- hotzone/urls.py
+++ hotzone/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('locations/', views.LocationList.as_view()),
+    path('locations/<int:pk>/', views.LocationDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
