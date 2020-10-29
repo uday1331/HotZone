f41c605ac2d80d9994668f0ba49ad27ab6d569cb
uday1331 <u3555276@connect.hku.hk>
2020-10-29 17:18:04 +0800

    server: add gunicorn

---
+++ server/README.md
@@ -0,0 +1,4 @@
+#Running the server
+
+1. Install all dependencies.
+2. run `gunicorn server.wsgi:application` from the `HotZone/server` directory. 
--- server/server/settings.py
+++ server/server/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'requests'
+    'requests',
+    'gunicorn'
 ]
 
 MIDDLEWARE = [
