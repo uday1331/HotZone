0ecb82ac0f3f91c6649bae65aadb9d5b2c901b5d
uday1331 <u3555276@connect.hku.hk>
2020-11-21 21:03:54 +0800

    server: add models to django admin

--- hotzone/admin.py
+++ hotzone/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Location
+from .models import Location, Virus, Patient, Case, Visit
 
 admin.site.register(Location)
+admin.site.register(Virus)
+admin.site.register(Patient)
+admin.site.register(Case)
+admin.site.register(Visit)
