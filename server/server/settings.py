732f5cebd41ee8dd49fdd8574431c6faeff508b2
uday1331 <u3555276@connect.hku.hk>
2020-11-22 23:10:44 +0800

    server: email based password reset

--- server/server/settings.py
+++ server/server/settings.py
@@ -157,3 +157,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(SERVER_DIR, 'build', 'static')
 
+# Django Auth Password Reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
