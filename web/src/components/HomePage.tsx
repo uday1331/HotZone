06c6e8f4cd2c66df50e55bc183392835f093bf63
Ahsan Syed <40141811+ahsan8244@users.noreply.github.com>
2020-11-22 23:38:31 +0800

    web: fix navbar consistency (#19)

--- web/src/components/HomePage.tsx
+++ web/src/components/HomePage.tsx
@@ -3,7 +3,7 @@ import { Layout, Menu } from "antd";
 import { EnvironmentOutlined, UserOutlined } from "@ant-design/icons";
 
 import { Logo } from "./Logo";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const { Content, Footer, Header } = Layout;
 
@@ -11,45 +11,50 @@ interface HomePageProps {
   children: React.ReactNode;
 }
 
-export const HomePage: React.FC<HomePageProps> = ({ children }) => (
-  <Layout style={{ minHeight: "100vh" }}>
-    <Header>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="0">
-            <Logo />
-          </Menu.Item>
-          <Menu.Item key="1" icon={<EnvironmentOutlined />}>
-            <Link to="/">
-              Locations
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<UserOutlined />}>
-            <Link to="/cases">
-              Cases
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UserOutlined />}>
-            <Link to="/patients">
-              Patients
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="4" icon={<UserOutlined />}>
-            <Link to="/settings">
-              Settings
-            </Link>
-          </Menu.Item>
-      </Menu>
-    </Header>
-    <Layout>
-      <Content style={{ margin: "24px 16px 0" }}>
-        <div
-          className="site-layout-background"
-          style={{ padding: 24, minHeight: 360 }}
-        >
-          {children}
-        </div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>Made with ♥️</Footer>
+export const HomePage: React.FC<HomePageProps> = ({ children }) => {
+  const location = useLocation();
+  const activeNav = location.pathname.split("/")[1] || "locations";
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header>
+        <Menu theme="dark" mode="horizontal" selectedKeys={[activeNav]}>
+            <Menu.Item key="0">
+              <Logo />
+            </Menu.Item>
+            <Menu.Item key="locations" icon={<EnvironmentOutlined />}>
+              <Link to="/">
+                Locations
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="cases" icon={<UserOutlined />}>
+              <Link to="/cases">
+                Cases
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="patients" icon={<UserOutlined />}>
+              <Link to="/patients">
+                Patients
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="settings" icon={<UserOutlined />}>
+              <Link to="/settings">
+                Settings
+              </Link>
+            </Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            {children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Made with ♥️</Footer>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  )
+};
