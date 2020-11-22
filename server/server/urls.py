223f715e76c10f34bf7a03b397bdf8eb7b04fe69
Adrio Alimin <strelka@connect.hku.hk>
2020-11-22 23:19:04 +0800

    Lex3 (#17)
    * Create CaseDetails.
    
    * Add locations visited to case details page.
    
    * web + server: Add case details functionality.
    
    * web + server: Add case details functionality.
    
    * web + server: redirect all non-API paths to React webapp.

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
 
 
 from .views import index
 
+# This, as you can see, is a hacky fix.
 urlpatterns = [
-    path('', index, name='index'),
     path('hotzone/', include('hotzone.urls')),
+    path('hotzone', include('hotzone.urls')),
     path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('login/', obtain_auth_token),
+    re_path(r'^(?!hotzone).*$', index, name="index")
 ]
