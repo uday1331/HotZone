5f891947d8ef24d155548c0ef51df479deb57b83
Adrio Alimin <strelka@connect.hku.hk>
2020-11-22 15:34:35 +0800

    Make frontend for listing cases.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
-import { HomePage, AddLocation, PatientTable } from "./components";
+import { HomePage, AddLocation, PatientTable, CasesList } from "./components";
 
 const App: FC = () => (
   <div className="App" style={{ height: "100vh" }}>
@@ -13,7 +13,7 @@ const App: FC = () => (
             <AddLocation />
           </Route>
           <Route exact path="/cases">
-            <p>cases</p>
+            <CasesList />
           </Route>
           <Route exact path="/patients">
             <PatientTable />
---
+++ web/src/components/CasesList.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Space, Spin, Table } from "antd";
+
+interface CaseType {
+  virus: string;
+  case_no: number;
+  confirmed: Date;
+  origin: string;
+  patient_hkid: string;
+  patient_fname: string;
+  patient_lname: string;
+  patient_locations_count: number;
+}
+
+const columns = [
+  {
+    title: "Pathogen",
+    dataIndex: "pathogen",
+    key: "pathogen",
+  },
+  {
+    title: "Case number",
+    dataIndex: "case_no",
+    key: "case_no",
+  },
+  {
+    title: "Confirmed on",
+    dataIndex: "confirmed",
+    key: "confirmed",
+  },
+  {
+    title: "Origin",
+    dataIndex: "origin",
+    key: "origin",
+  },
+  {
+    title: "First Name",
+    dataIndex: "patient_fname",
+    key: "patient_fname",
+  }, 
+  {
+    title: "Last Name",
+    dataIndex: "patient_lname",
+    key: "patient_lname",
+  },
+];
+
+export const CasesList: React.FC = () => {
+  const [cases, setCases] = useState<Array<CaseType>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    const fetchCasesList = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("https://group-q-hotzone.herokuapp.com/hotzone/cases.json");
+        const casesList: Array<CaseType> = response.data;
+        setCases(casesList);
+        setLoading(false);
+        setError("");
+      } catch(e) {
+        setCases([]);
+        setError("failed to fetch cases data");
+        setLoading(false);
+      }
+    }
+
+    fetchCasesList();
+  }, [])
+
+  if (error && error !== "") {
+    return <p>{error}</p>
+  }
+
+  if (loading) {
+    return (
+      <Space size="middle">
+        <Spin size="large" />
+      </Space>
+    );
+  }
+
+  return (
+    cases && (
+      <Table
+        dataSource={cases}
+        columns={columns}
+      />
+    )
+  );
+}
