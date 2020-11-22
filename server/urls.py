01c617ba4e8bad9146287e8e4a156878701025b2
Uday Jain <47697048+uday1331@users.noreply.github.com>
2020-11-22 23:32:44 +0800

    server: email based password reset (#18)

--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.auth import views as auth_views
 
 
 from .views import index
@@ -12,5 +13,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin', admin.site.urls),
     path('login/', obtain_auth_token),
-    re_path(r'^(?!hotzone).*$', index, name="index")
+    path("password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),    
+    re_path(r'^(?!hotzone).*$', index, name="index"),
 ]
