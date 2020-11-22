214b1ab1d82543e610c0cf544c3adc9f1dfb57ff
uday1331 <u3555276@connect.hku.hk>
2020-11-22 21:17:55 +0800

    server+web: change location of auth url

--- hotzone/urls.py
+++ hotzone/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
@@ -10,7 +11,6 @@ urlpatterns = [
     path('locations/<str:name>/', views.LocationSearchFromGeoData.as_view()),
     path('patients/', views.PatientList.as_view()),
     path('cases/', views.CasesList.as_view()),
-    path('login/', obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
